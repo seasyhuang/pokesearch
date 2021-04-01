78a9015c74e95ffb1eb99566b20ed2241576e571
Seasy <seasy.huang@gmail.com>
2021-04-01 10:23:41 -0400

    adding native base, background image, more styles

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Platform } from 'react-native';
+import { Button } from 'native-base';
 
+// const image = require(./assests/icons/whatever_image_here.jpg);
 const image = { uri: "https://cdn.dribbble.com/users/228053/screenshots/2760950/starters3.jpg" };
+
 
 export default function App() {
   return (
@@ -10,14 +13,19 @@ export default function App() {
       <ImageBackground
         source={image}
         style={styles.image}
-        resizeMode='contain'
-      >
-        <Text
-          style = {
-            { marginBottom: 120 }
-          }
-        >
-        Open up App.js to start working on your app!</Text>
+        resizeMode='contain'>
+
+          <View style={styles.viewStyle}>
+            <Text style={styles.titleStyle}>Welcome to Pokesearch</Text>
+            <Button
+                block={true}
+                style={styles.buttonStyle}
+                onPress={() => {}}
+              >
+              <Text style={styles.buttonText}>Start Searching</Text>
+            </Button>
+          </View>
+
         <StatusBar style="auto" />
       </ImageBackground>
 
@@ -32,6 +40,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFAEA',
     alignItems: 'stretch',
     justifyContent: 'center',
+    marginTop: Platform.OS === "android" ? 24 : 0,
   },
   image: {
     flex: 1,
@@ -40,4 +49,23 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  viewStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 500,
+  },
+  titleStyle: {
+    fontSize: 30,
+    color: 'blue',
+    alignItems: 'center',
+  },
+  buttonStyle: {
+    margin: 10,
+    color: 'blue'
+  },
+  buttonText: {
+    color: 'white'
+  }
 });
