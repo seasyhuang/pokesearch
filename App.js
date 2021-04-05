809d8d2e21ea1e0726fd4d4f9f4464943a76326e
Seasy <seasy.huang@gmail.com>
2021-04-05 09:33:52 -0400

    Creating Landing component

--- App.js
+++ App.js
@@ -1,34 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground, Platform } from 'react-native';
-import { Button } from 'native-base';
 
-// const image = require(./assests/icons/whatever_image_here.jpg);
-const image = { uri: "https://cdn.dribbble.com/users/228053/screenshots/2760950/starters3.jpg" };
+import Landing from './src/Landing';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={image}
-        style={styles.image}
-        resizeMode='contain'>
-
-          <View style={styles.viewStyle}>
-            <Text style={styles.titleStyle}>Welcome to Pokesearch</Text>
-            <Button
-                block={true}
-                style={styles.buttonStyle}
-                onPress={() => {}}
-              >
-              <Text style={styles.buttonText}>Start Searching</Text>
-            </Button>
-          </View>
-
-        <StatusBar style="auto" />
-      </ImageBackground>
-
+      <Landing/>
+      
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -42,30 +24,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: Platform.OS === "android" ? 24 : 0,
   },
-  image: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: "center",
-    width: '100%',
-    height: '100%',
-  },
-  viewStyle: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 500,
-  },
-  titleStyle: {
-    fontSize: 30,
-    color: 'blue',
-    alignItems: 'center',
-  },
-  buttonStyle: {
-    margin: 10,
-    color: 'blue'
-  },
-  buttonText: {
-    color: 'white'
-  }
+
 });
---
+++ src/Landing.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View, Text, ImageBackground } from 'react-native';
+import { Button } from 'native-base';
+
+
+// const image = require(./assests/icons/whatever_image_here.jpg);
+const image = { uri: "https://cdn.dribbble.com/users/228053/screenshots/2760950/starters3.jpg" };
+
+class Landing extends React.Component{
+  render () {
+    return (
+      <View>
+        <ImageBackground
+          source={image}
+          style={styles.image}
+          resizeMode='contain'>
+
+            <View style={styles.viewStyle}>
+              <Text style={styles.titleStyle}>Welcome to Pokesearch</Text>
+              <Button
+                  block={true}
+                  style={styles.buttonStyle}
+                  onPress={() => {}}
+                >
+                <Text style={styles.buttonText}>Start Searching</Text>
+              </Button>
+            </View>
+        </ImageBackground>
+      </View>
+    )
+  }
+}
+
+export default Landing;
+
+const styles = {
+  image: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: "center",
+    width: '100%',
+    height: '100%',
+  },
+  viewStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 500,
+  },
+  titleStyle: {
+    fontSize: 30,
+    color: 'blue',
+    alignItems: 'center',
+  },
+  buttonStyle: {
+    margin: 10,
+    color: 'blue'
+  },
+  buttonText: {
+    color: 'white'
+  }
+}
