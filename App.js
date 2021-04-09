6470eed52991e858d67e25d63b8b96889ec4bcd8
Seasy <seasy.huang@gmail.com>
2021-04-09 09:32:24 -0400

    Add abilities and background image

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, Platform } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 import Landing from './src/Landing';
 import Search from './src/Search';
--- src/SearchBody.js
+++ src/SearchBody.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ScrollView, View, Text, Image } from 'react-native'
+import { ScrollView, View, Text, Image, ImageBackground, Dimensions } from 'react-native'
 import { ListItem, List } from 'native-base'
+
+let height = Dimensions.get('window').height;
+let width = Dimensions.get('window').width;
 
 class SearchBody extends React.Component {
   render () {
@@ -12,35 +15,61 @@ class SearchBody extends React.Component {
       return <View></View>
     }
     return(
-      <ScrollView style = {{ flex: 1 }}>
-        <Text style = { styles.header } >#{pokemon.id} - {pokemon.name.toUpperCase()}</Text>
-        <View style = { styles.viewStyle } >
-          <Image
-            source = {{ uri: pokemon.sprites.front_default }}
-            style = { styles.img }
-          />
-        </View>
-        <View style = { styles.info } >
-          <ListItem itemDivider>
-            <Text style = {{ fontWeight: 'bold' }}>Size:</Text>
-          </ListItem>
-          <ListItem itemDivider>
-            <Text>Weight: { pokemon.weight/10 } kg | { Math.round((pokemon.weight/10 * 2.205) * 100)/100 } lbs</Text>
-          </ListItem>
-          <ListItem itemDivider>
-            <Text>Height: { pokemon.height/10 } m | { Math.round((pokemon.height/10* 39.37) * 100)/100 } in</Text>
-          </ListItem>
-        </View>
-      </ScrollView>
+      <ImageBackground
+          style = { styles.backgroundImage }
+          source = {{ uri: "http://pokemongolive.com/img/posts/raids_loading.png" }}>
+          <ScrollView style = {{ flex: 1 }}>
+            <Text style = { styles.header } >#{pokemon.id} - {pokemon.name.toUpperCase()}</Text>
+            <View style = { styles.viewStyle } >
+              <Image
+                source = {{ uri: pokemon.sprites.front_default }}
+                style = { styles.img }
+              />
+            </View>
+            <View style = { styles.info } >
+              <ListItem itemDivider>
+                <Text style = {{ fontWeight: 'bold' }}>Size</Text>
+              </ListItem>
+              <ListItem>
+                <Text>Weight: { pokemon.weight/10 } kg | { Math.round((pokemon.weight/10 * 2.205) * 100)/100 } lbs</Text>
+              </ListItem>
+              <ListItem>
+                <Text>Height: { pokemon.height/10 } m | { Math.round((pokemon.height/10* 39.37) * 100)/100 } in</Text>
+              </ListItem>
+
+              <ListItem itemDivider>
+                <Text style = {{ fontWeight: 'bold' }}>Abilities</Text>
+              </ListItem>
+              <List
+                dataArray = { pokemon.abilities }
+                renderRow = { (item) =>
+                  <ListItem>
+                    <Text>{ item.ability.name }</Text>
+                  </ListItem>
+                }
+              >
+              </List>
+            </View>
+          </ScrollView>
+      </ImageBackground>
     )
   }
 }
 
 const styles = {
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    height: height,
+    width: width
+  },
   header: {
     fontSize: 30,
+    opacity: 0.92,
     color: 'red',
-    flex: 1
+    backgroundColor: 'white',
+		textAlign: 'center',
+    padding: '10px'
   },
   viewStyle: {
     justifyContent: 'center',
@@ -49,8 +78,8 @@ const styles = {
   },
   img: {
     // https://stackoverflow.com/questions/39631895/how-to-set-image-width-to-be-100-and-height-to-be-auto-in-react-native
-    height: 100,
-    width: 100,
+    height: 200,
+    width: 200,
     justifyContent: 'center',
     alignItems: 'center',
   },
